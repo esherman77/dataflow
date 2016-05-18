be8df299abd6a22547488eba599e27cb8561aee8
Elena Sherman <esherman@cse.unl.edu>
2016-05-18 10:55:09 -0600

    updates for mvn build

---
+++ my-test/src/main/java/com/test/app/App.java
@@ -0,0 +1,37 @@
+package com.test.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+
+		public int m(int y){
+			int z = 0; 
+			while (y > 0){
+				z = z + 1;
+				y = y - 1;
+			}
+			return z;
+		}
+		
+		public int n(){
+			int y = 0;
+			return m(y);
+		}
+		
+		public int o(){
+			int y = 1;
+			return m(y);
+		}
+		public static void main(String[] args) {
+			App t = new App();
+			int result1 = t.o()/t.n();
+			int result2 = t.n()/t.o();
+			System.out.println("Hello world!" + result1 + " " + result2);
+
+		}
+		
+
+}
