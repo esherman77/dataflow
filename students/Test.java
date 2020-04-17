78b1ed7dda8633249b445d24aa849e450fd8eba3
esherman77 <elenasherman@boisestate.edu>
2020-04-17 10:01:18 -0600

    changes

--- students/Test.java
+++ students/Test.java
@@ -25,10 +25,20 @@ public class Test
 			int y = 0;
 			return m(y);
 		}
+		
+		public int k(int x){
+			if(x > 0){
+				x = 2*x;
+			} else {
+				x = 2*x - 1;
+			}
+			return x;
+		}
 		public static void main(String[] args) {
 			Test t = new Test();
+			int x = Integer.parseInt(args[0]);
 			int result1 = t.o()/t.n();
-			int result2 = t.n()/t.o();
+			int result2 = t.n()/t.k(x);
 			System.out.println("Hello world" + result1 + " " + result2);
 
 		}
